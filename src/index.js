209f7a4473015913ad6e361b6e682e6a4d1cfd17
Abdullah-1904 <abbasibro.ab@gmail.com>
2025-05-02 17:03:42 +0500

    routing changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import RootLayout from "./RootLayout"; // The layout component with the Navbar
+import { HashRouter, Routes, Route } from "react-router-dom";
+import RootLayout from "./RootLayout";
 
 import App from "./App";
 import About from "./pages/About";
@@ -15,60 +15,29 @@ import ErrorPage from "./pages/ErrorPage";
 import Ultrasound from "./tests/Ultrasound";
 import ECG from "./tests/ECG";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />, // Navbar with Outlet Element inside this Element
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        // index: true => means if user goes to "/", render <App />
-        // The index route (index: true) means if the user goes exactly to /, we render <App />.
-
-        // path: "/",
-        index: true,
-        element: <App />,
-      },
-      {
-        path: "about",
-        element: <About />,
-      },
-      {
-        path: "contact",
-        element: <Contact />,
-      },
-      {
-        // A parent route for "Test and Services"
-        path: "test-and-services",
-        element: <TestAndServices />,
-        // Nested routes under "test-and-services"
-        children: [
-          {
-            path: "blood-test",
-            element: <BloodCP />,
-          },
-          {
-            path: "special-chemistry",
-            element: <SpecialChemistry />,
-          },
-          {
-            path: "ultrasound",
-            element: <Ultrasound />,
-          },
-          {
-            path: "ecg",
-            element: <ECG />,
-          },
-        ],
-      },
-    ],
-  },
-]);
-
-// 2) Render the RouterProvider with our custom router
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <RootLayout>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/test-and-services" element={<TestAndServices />} />
+          <Route path="/test-and-services/blood-test" element={<BloodCP />} />
+          <Route
+            path="/test-and-services/special-chemistry"
+            element={<SpecialChemistry />}
+          />
+          <Route
+            path="/test-and-services/ultrasound"
+            element={<Ultrasound />}
+          />
+          <Route path="/test-and-services/ecg" element={<ECG />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </RootLayout>
+    </HashRouter>
   </React.StrictMode>
 );
