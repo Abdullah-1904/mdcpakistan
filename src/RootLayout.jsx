7cbb98cc20febbd59c5a67b862e1e3411516bd15
Abdullah-1904 <abbasibro.ab@gmail.com>
2025-05-02 17:12:04 +0500

    ..

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -4,30 +4,21 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-// function RootLayout() {
-//   return (
-//     <>
-//       {/* this is root layout which contains Navbar. This will be called in index.js  */}
-//       <Navbar />
-//       <main className="pt-16">
-//         {/*  i put a div so that any text on any page would affect the navbar  */}
-//         <Outlet />
-//       </main>
-//       {/* //everything else will be rendered inside this Outlet */}
-//       <div className="mt-5">
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// }
-const RootLayout = () => {
+function RootLayout() {
   return (
     <>
+      {/* this is root layout which contains Navbar. This will be called in index.js  */}
       <Navbar />
-      <Outlet />
-      <Footer />
+      <main className="pt-16">
+        {/*  i put a div so that any text on any page would affect the navbar  */}
+        <Outlet />
+      </main>
+      {/* //everything else will be rendered inside this Outlet */}
+      <div className="mt-5">
+        <Footer />
+      </div>
     </>
   );
-};
+}
 
 export default RootLayout;
